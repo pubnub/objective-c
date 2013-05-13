83ba18adad2e4677ad39a88c1c17f8f072e1de22
gcohen <geremy@pubnub.com>
2013-05-13 15:15:09 -0700

    Removing uneeded PNObserver code from demo.

--- iOS/HOWTO/HelloWorld/3.4/PubNubDemo/ViewController.m
+++ iOS/HOWTO/HelloWorld/3.4/PubNubDemo/ViewController.m
@@ -118,12 +118,6 @@
 - (IBAction)disconnectButtonTapped:(id)sender {
     [PubNub disconnect];
 
-
-    [[PNObservationCenter defaultCenter] removeTimeTokenReceivingObserver:self];
-    [[PNObservationCenter defaultCenter] removeClientConnectionStateObserver:self];
-    [[PNObservationCenter defaultCenter] removeChannelParticipantsListProcessingObserver:self];
-    [[PNObservationCenter defaultCenter] removePresenceEventObserver:self];
-
     self.subscribeButton.enabled = NO;
     self.sendMessageButton.enabled = NO;
 }
