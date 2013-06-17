7c9cd8dd8741070009d476fa1abab38929d7d6da
gcohen <geremy@pubnub.com>
2013-06-16 21:29:45 -0700

    change to channel a

--- iOS/HOWTO/SimpleSubscribe/3.4/PubNubDemo/ViewController.m
+++ iOS/HOWTO/SimpleSubscribe/3.4/PubNubDemo/ViewController.m
@@ -30,7 +30,7 @@
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC); dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
 
             // then subscribe on channel a
-            PNChannel *myChannel = [PNChannel channelWithName:@"b" shouldObservePresence:YES];
+            PNChannel *myChannel = [PNChannel channelWithName:@"a" shouldObservePresence:YES];
             [PubNub subscribeOnChannel:myChannel];
 
         }); }
