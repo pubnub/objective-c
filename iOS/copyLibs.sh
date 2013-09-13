16250d4dd0887f5761802c945d101d7d453bb7e1
gcohen <geremy@pubnub.com>
2013-09-13 16:53:21 -0700

    removing old dirs and fixing copyLibs.sh

--- iOS/copyLibs.sh
+++ iOS/copyLibs.sh
@@ -1,8 +1,8 @@
-cp -r 3.4/pubnub/libs/PubNub/* HOWTO/HelloWorld/3.4/PubNubDemo/libs/PubNub
-cp -r 3.4/pubnub/libs/PubNub/* HOWTO/SimpleSubscribe/3.4/PubNubDemo/libs/PubNub
-cp -r 3.4/pubnub/libs/PubNub/* HOWTO/APNSVideo/demo9/demo9/libs/PubNub
-cp -r 3.4/pubnub/libs/PubNub/* HOWTO/ULSMethods/ULSMethods/libs/PubNub
-cp -r 3.4/pubnub/libs/PubNub/* ../cocoa/3.4/PubNubMacOSTestApplication/PubNubMacOSTestApplication/libs/PubNub
-cp -r 3.4/pubnub/libs/PubNub/* ../cocoa/HOWTO/SimpleSubscribe/PubNubMacOSTestApplication/libs/PubNub
+cp -r pubnub/libs/PubNub/* HOWTO/HelloWorld/PubNubDemo/libs/PubNub
+cp -r pubnub/libs/PubNub/* HOWTO/SimpleSubscribe/PubNubDemo/libs/PubNub
+cp -r pubnub/libs/PubNub/* HOWTO/APNSVideo/demo9/demo9/libs/PubNub
+cp -r pubnub/libs/PubNub/* HOWTO/ULSMethods/ULSMethods/libs/PubNub
+cp -r pubnub/libs/PubNub/* ../cocoa/PubNubMacOSTestApplication/PubNubMacOSTestApplication/libs/PubNub
+cp -r pubnub/libs/PubNub/* ../cocoa/HOWTO/SimpleSubscribe/PubNubMacOSTestApplication/libs/PubNub
 
 cp -r 3.4/pubnub/libs/PubNub/* HOWTO/CallsWithoutBlocks/CallsWithoutBlocks/libs/PubNub
