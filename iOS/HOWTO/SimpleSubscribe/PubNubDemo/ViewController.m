f73096b745043ffd8be0ef4ba7e0bae8c3c64ebf
gcohen <geremy@pubnub.com>
2013-09-18 12:58:27 -0700

    initial commit

--- iOS/HOWTO/SimpleSubscribe/PubNubDemo/ViewController.m
+++ iOS/HOWTO/SimpleSubscribe/PubNubDemo/ViewController.m
@@ -20,78 +20,50 @@
 {
     [super viewDidLoad];
 
-    [PubNub setClientIdentifier:@"SimpleSubscribe"];
-    //[uuidView setText:[NSString stringWithFormat:@"%@", [PubNub clientIdentifier]]];
-
-    [[PNObservationCenter defaultCenter] addMessageReceiveObserver:self
-                                                         withBlock:^(PNMessage *message) {
-
-                                                             NSLog(@"Text Length: %i", textView.text.length);
-
-                                                             if (textView.text.length > 2000) {
-                                                                 [textView setText:@""];
-                                                             }
-
-                                                             [textView setText:[message.message stringByAppendingFormat:@"\n%@\n", textView.text]];
-
-                                                         }];
-
-    [[PNObservationCenter defaultCenter] addPresenceEventObserver:self withBlock:^(PNPresenceEvent *event) {
-
-        NSString *eventString;
-        if (event.type == PNPresenceEventJoin) {
-            eventString = @"Join";
-        } else
-        if (event.type == PNPresenceEventLeave) {
-            eventString = @"Leave";
-        } else
-        if (event.type == PNPresenceEventTimeout) {
-            eventString = @"Timeout";
-        }
-
-        eventString = [NSString stringWithFormat:@"%@ : %@", event.uuid, eventString];
-
-        [presenceView setText:[eventString stringByAppendingFormat:@"\n%@\n", presenceView.text]];
 
 
-
-    }];
-
-
-    // Do any additional setup after loading the view, typically from a nib.
-
-    // amongst other things, set the sub/pub keys to demo
     [PubNub setConfiguration:[PNConfiguration defaultConfiguration]];
+
     [PubNub connectWithSuccessBlock:^(NSString *origin) {
 
         PNLog(PNLogGeneralLevel, self, @"{BLOCK} PubNub client connected to: %@", origin);
 
-        // wait 1 second
-        int64_t delayInSeconds = 1.0;
-        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC); dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    } errorBlock:^(PNError *connectionError) {
 
-            // then subscribe on channel a
-            PNChannel *myChannel = [PNChannel channelWithName:@"a" shouldObservePresence:YES];
-            [PubNub subscribeOnChannel:myChannel];
+        UIAlertView *connectionErrorAlert = [UIAlertView new];
 
-        }); }
-     // In case of error you always can pull out error code and identify what happened and what you can do // additional information is stored inside error's localizedDescription, localizedFailureReason and
-     // localizedRecoverySuggestion)
-                         errorBlock:^(PNError *connectionError) {
-                             if (connectionError.code == kPNClientConnectionFailedOnInternetFailureError) {
-                                 PNLog(PNLogGeneralLevel, self, @"Connection will be established as soon as internet connection will be restored");
-                             }
+        connectionErrorAlert.title = [NSString stringWithFormat:@"%@(%@)",
 
-                             UIAlertView *connectionErrorAlert = [UIAlertView new];
-                             connectionErrorAlert.title = [NSString stringWithFormat:@"%@(%@)",
-                                                                                     [connectionError localizedDescription],
-                                                                                     NSStringFromClass([self class])];
-                             connectionErrorAlert.message = [NSString stringWithFormat:@"Reason:\n%@\n\nSuggestion:\n%@",
-                                                                                       [connectionError localizedFailureReason],
-                                                                                       [connectionError localizedRecoverySuggestion]];
-                             [connectionErrorAlert addButtonWithTitle:@"OK"];
-                             [connectionErrorAlert show];
-                         }];
+                                                                [connectionError localizedDescription
+                                                                ],
+
+                                                                NSStringFromClass([self class])];
+
+        connectionErrorAlert.message = [NSString stringWithFormat:@"SetUpPubnub Reason:\n%@\n\nSuggestion:\n%@",
+
+        [connectionError localizedFailureReason],
+
+        [connectionError localizedRecoverySuggestion]];
+
+        [connectionErrorAlert addButtonWithTitle:@"OK"];
+
+        [connectionErrorAlert show];
+
+    }];
+
+    PNChannel *defaultChannel = [PNChannel channelWithName:@"zzz" shouldObservePresence:YES];
+
+    [PubNub subscribeOnChannel:defaultChannel withCompletionHandlingBlock:^(
+            PNSubscriptionProcessState state, NSArray *channels, PNError *subscriptionError) {
+
+    }];
+
+// Uncomment this setClientIdentifier line to reproduce issue
+
+[PubNub setClientIdentifier:@"foo"];
+
+    [PubNub sendMessage:@"hi" toChannel:defaultChannel];
+
 }
 
 - (void)didReceiveMemoryWarning {
