8110b421aea6d58f8c96036e9ef7e5003c9ce561
Valentin Tuller <tullerval@gmail.com>
2013-10-01 17:49:00 +0300

    add app for test reconnect time.
    https://trello.com/c/Eu8yT2D7/64-add-test-connection-issues-using-portable-ap-mifi

---
+++ iOS/tests/ConnectionIssues/pubnub/libs/PubNub/Network/Transport/PNConnection+Protected.h
@@ -0,0 +1,68 @@
+//
+//  PNConnection+Protected.h
+//  pubnub
+//
+//  This header file used by library internal
+//  components which require to access to some
+//  methods and properties which shouldn't be
+//  visible to other application components
+//
+//  Created by Sergey Mamontov.
+//
+//
+
+#import "PNConnection.h"
+#import "PNBaseRequest+Protected.h"
+
+
+#pragma mark - Structures
+
+// Structure describes list of available connection identifiers
+struct PNConnectionIdentifiersStruct {
+    
+    // Used to identify connection which is used for: subscriptions and presence observing
+    __unsafe_unretained NSString *messagingConnection;
+    
+    // Used for another set of calls to the PubNub service
+    __unsafe_unretained NSString *serviceConnection;
+};
+
+extern struct PNConnectionIdentifiersStruct PNConnectionIdentifiers;
+
+
+@interface PNConnection (Protected)
+
+
+#pragma mark - Class methods
+
++ (void)resetConnectionsPool;
+
+
+#pragma mark - Instance methods
+
+/**
+ * Open connection from name of the user (if flag is set to 'YES')
+ */
+- (BOOL)connectByInternalRequest;
+- (BOOL)isConnecting;
+- (BOOL)isReconnecting;
+- (BOOL)shouldReconnect;
+
+- (BOOL)isSuspending;
+- (BOOL)isResuming;
+
+/**
+ * Disconnect from name of the user (if flag is set to 'YES')
+ */
+- (void)disconnectByInternalRequest;
+- (BOOL)isDisconnecting;
+
+/**
+ * This is final point where connection can release all resources which may not allow it to destroy it from outside
+ */
+- (void)prepareForTermination;
+
+#pragma mark -
+
+
+@end
