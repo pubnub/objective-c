9f322bd92ab1238e5185f85932aaa1d9af108725
gcohen <geremy@pubnub.com>
2013-10-02 17:27:35 -0700

    mac version of ClientSidePresence

---
+++ cocoa/HOWTO/ClientSidePresence/PubNubMacOSTestApplication/Classes/Controller/TAAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  TAAppDelegate.h
+//  PubNubMacOSTestApplication
+//
+//  Created by Sergey Mamontov on 4/22/13.
+//  Copyright (c) 2013 Sergey Mamontov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+@property PNChannel *myChannel;
+
+@property NSTimer *pingPongTimer;
+@property NSString *presenceChannel;
+@property NSMutableDictionary *occupants;
+
+@end
---
+++ cocoa/HOWTO/ClientSidePresence/PubNubMacOSTestApplication/libs/PubNub/Data/Parsers/PNTimeTokenResponseParser.m
@@ -0,0 +1,90 @@
+//
+//  PNTimeTokenResponseParser.h
+// 
+//
+//  Created by moonlight on 1/15/13.
+//
+//
+
+
+#import "PNTimeTokenResponseParser.h"
+#import "PNResponse.h"
+
+
+// ARC check
+#if !__has_feature(objc_arc)
+#error PubNub time token response parser must be built with ARC.
+// You can turn on ARC for only PubNub files by adding '-fobjc-arc' to the build phase for each of its files.
+#endif
+
+
+#pragma mark Private interface methods
+
+@interface PNTimeTokenResponseParser ()
+
+
+#pragma mark - Properties
+
+@property (nonatomic, strong) NSNumber *timeToken;
+
+
+#pragma mark - Instance methods
+
+/**
+ * Returns reference on initialized parser for concrete
+ * response
+ */
+- (id)initWithResponse:(PNResponse *)response;
+
+
+@end
+
+
+#pragma mark Public interface methods
+
+@implementation PNTimeTokenResponseParser
+
+
+#pragma mark - Class methods
+
++ (id)parserForResponse:(PNResponse *)response {
+
+    NSAssert1(0, @"%s SHOULD BE CALLED ONLY FROM PARENT CLASS", __PRETTY_FUNCTION__);
+
+
+    return nil;
+}
+
+
+#pragma mark - Instance methods
+
+- (id)initWithResponse:(PNResponse *)response {
+
+    // Check whether initialization successful or not
+    if ((self = [super init])) {
+
+        id timeToken = [(NSArray *)response.response lastObject];
+        self.timeToken = PNNumberFromUnsignedLongLongString(timeToken);
+    }
+
+
+    return self;
+}
+
+- (id)parsedData {
+
+    return self.timeToken;
+}
+
+- (NSString *)description {
+
+    return [NSString stringWithFormat:@"%@ (%p): <time token: %@>",
+                    NSStringFromClass([self class]),
+                    self,
+                    self.timeToken];
+}
+
+#pragma mark -
+
+
+@end
