6a2d4519960838c2c80f1a836491ed55091783dd
Sergey Mamontov <parfeon@me.com>
2013-10-14 17:05:09 +0300

    * copied library into demo projects * updated library information (debug output in console)

--- iOS/tests/pubnubBackground/pubnubBackground/libs/PubNub/Core/PubNub.m
+++ iOS/tests/pubnubBackground/pubnubBackground/libs/PubNub/Core/PubNub.m
@@ -38,7 +38,7 @@
 
 static NSString * const kPNLibraryVersion = @"3.5.1";
 static NSString * const kPNCodebaseBranch = @"hotfix-t175";
-static NSString * const kPNCodeCommitIdentifier = @"b96dcb8cfb81cab2483edb661e2331010853f1cb";
+static NSString * const kPNCodeCommitIdentifier = @"adfe0b085e25344227da42aefc34bd6fda702cd0";
 
 
 // Stores reference on singleton PubNub instance
