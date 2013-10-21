1ab6093019e61ce006e5d1c6ef88485614cbe765
Valentin Tuller <tullerval@gmail.com>
2013-10-22 00:23:08 +0300

    add BackgroundAudio app

---
+++ iOS/HOWTO/BackgroundAudio/pubnub/libs/PubNub/Data/PNDate.m
@@ -0,0 +1,105 @@
+//
+//  PNDate.m
+//  pubnub
+//
+//  Created by Sergey Mamontov on 04/01/13.
+//
+//
+
+
+// ARC check
+#if !__has_feature(objc_arc)
+#error PubNub date must be built with ARC.
+// You can turn on ARC for only PubNub files by adding '-fobjc-arc' to the build phase for each of its files.
+#endif
+
+
+#pragma mark Private interface declaration
+
+@interface PNDate ()
+
+
+#pragma mark - Properties
+
+@property (nonatomic, strong) NSNumber *timeToken;
+@property (nonatomic, strong) NSDate *date;
+
+
+#pragma mark - Instance methods
+
+- (id)initWithTimeToken:(NSNumber *)timeToken;
+
+
+@end
+
+
+#pragma mark - Public interface implementation
+
+@implementation PNDate
+
+
+#pragma mark - Class methods
+
++ (instancetype)dateWithDate:(NSDate *)date {
+    
+    PNDate *dateObject = nil;
+    if (date != nil) {
+        
+        dateObject = [[self alloc] initWithTimeToken:PNTimeTokenFromDate(date)];
+    }
+    
+
+    return dateObject;
+}
+
++ (instancetype)dateWithToken:(NSNumber *)number {
+    
+    PNDate *dateObject = nil;
+    if (number != nil) {
+        
+        dateObject = [[self alloc] initWithTimeToken:number];
+    }
+
+    
+    return dateObject;
+}
+
+
+#pragma mark - Instance methods
+
+- (id)initWithTimeToken:(NSNumber *)timeToken {
+
+    // Check whether initialization successfull or not
+    if ((self = [super init])) {
+
+        self.timeToken = timeToken;
+    }
+
+
+    return self;
+}
+
+- (NSDate *)date {
+
+    if (_date == nil) {
+
+        _date = [NSDate dateWithTimeIntervalSince1970:PNUnixTimeStampFromTimeToken(self.timeToken)];
+    };
+
+
+    return _date;
+}
+
+- (NSString *)description {
+
+    return [NSString stringWithFormat:@"%@ (%p) <date: %@; time token: %@>",
+                    NSStringFromClass([self class]),
+                    self,
+                    self.date,
+                    self.timeToken];
+}
+
+#pragma mark -
+
+
+@end
