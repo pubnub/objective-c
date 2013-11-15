27b87ac4233db494b4e27f04cee6d9d3abefeb5d
Sergey Mamontov <parfeon@me.com>
2013-11-15 20:19:53 +0200

    * copied library into demo projects  * updated library information (debug output in console)

--- cocoa/HOWTO/SimpleSubscribe/PubNubMacOSTestApplication/libs/PubNub/Core/PubNub.m
+++ cocoa/HOWTO/SimpleSubscribe/PubNubMacOSTestApplication/libs/PubNub/Core/PubNub.m
@@ -38,7 +38,7 @@
 
 static NSString * const kPNLibraryVersion = @"3.5.2";
 static NSString * const kPNCodebaseBranch = @"3.5.2";
-static NSString * const kPNCodeCommitIdentifier = @"dc488e7655b17df7f30607d14b8814a71e0dadee";
+static NSString * const kPNCodeCommitIdentifier = @"c90ae4363996c1622c771082f18acb6541281bfd";
 
 // Stores reference on singleton PubNub instance
 static PubNub *_sharedInstance = nil;
