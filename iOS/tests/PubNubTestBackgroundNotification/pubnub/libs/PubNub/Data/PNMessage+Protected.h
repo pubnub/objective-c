5e5ee502165ed1f6c3f99cc174d8229e399ccda4
Valentin Tuller <tullerval@gmail.com>
2014-01-29 16:55:57 +0200

    add test

---
+++ iOS/tests/PubNubTestBackgroundNotification/pubnub/libs/PubNub/Data/PNMessage+Protected.h
@@ -0,0 +1,91 @@
+//
+//  PNMessage+Protected.h
+//  pubnub
+//
+//  This header file used by library internal
+//  components which require to access to some
+//  methods and properties which shouldn't be
+//  visible to other application components
+//
+//  Created by Sergey Mamontov.
+//
+//
+
+#import "PNMessage.h"
+
+
+#pragma mark Struct
+
+struct PNMessageDataKeysStruct {
+
+    /**
+     Stores key under which message will be encoded.
+     */
+    __unsafe_unretained NSString *message;
+
+    /**
+     Stores key under which channel name will be encoded.
+     */
+    __unsafe_unretained NSString *channel;
+
+    /**
+     Stores key under which message receive will be encoded.
+     */
+    __unsafe_unretained NSString *date;
+};
+
+extern struct PNMessageDataKeysStruct PNMessageDataKeys;
+
+
+#pragma mark - Class forward
+
+@class PNChannel, PNDate;
+
+
+#pragma mark - Protected methods
+
+@interface PNMessage (Protected)
+
+
+#pragma mark - Properties
+
+// Stores reference on message body
+@property (nonatomic, strong) id message;
+
+// Stores reference on channel to which this message
+// should be sent
+@property (nonatomic, strong) PNChannel *channel;
+
+// Stores reference on date when this message was received
+// (doesn't work for history, only for presence events)
+@property (nonatomic, strong) PNDate *receiveDate;
+
+
+
+#pragma mark - Class methods
+
+/**
+ * Return reference on object data object initialized with
+ * object object and target channel
+ * Message should be in stringified JSON format
+ */
++ (PNMessage *)messageWithObject:(id)object forChannel:(PNChannel *)channel error:(PNError **)error;
+
+/**
+ * Return reference on message data object which will represent
+ * message received from PubNub service
+ */
++ (PNMessage *)messageFromServiceResponse:(id)messageBody onChannel:(PNChannel *)channel atDate:(PNDate *)messagePostDate;
+
+
+#pragma mark - Instance methods
+
+/**
+ * Initialize object instance with text and channel
+ */
+- (id)initWithObject:(id)object forChannel:(PNChannel *)channel;
+
+- (void)setReceiveDate:(PNDate *)receiveDate;
+
+
+@end
---
+++ iOS/tests/PubNubTestBackgroundNotification/pubnub/libs/PubNub/Network/Packets/PNMessagePostRequest.m
@@ -0,0 +1,175 @@
+//
+//  PNSubscribeRequest.m
+//  pubnub
+//
+//  This request object is used to describe
+//  message sending request which will be
+//  scheduled on requests queue and executed
+//  as soon as possible.
+//
+//
+//  Created by Sergey Mamontov on 12/28/12.
+//
+//
+
+#import "PNMessagePostRequest.h"
+#import "PNServiceResponseCallbacks.h"
+#import "PNBaseRequest+Protected.h"
+#import "NSString+PNAddition.h"
+#import "PNMessage+Protected.h"
+#import "PNChannel+Protected.h"
+#import "PubNub+Protected.h"
+#import "PNCryptoHelper.h"
+#import "PNConstants.h"
+
+
+// ARC check
+#if !__has_feature(objc_arc)
+#error PubNub message post request must be built with ARC.
+// You can turn on ARC for only PubNub files by adding '-fobjc-arc' to the build phase for each of its files.
+#endif
+
+
+#pragma mark Private interface methods
+
+@interface PNMessagePostRequest ()
+
+
+#pragma mark - Properties
+
+// Stores reference on message object which will
+// be processed
+@property (nonatomic, strong) PNMessage *message;
+
+// Stores reference on prepared message
+@property (nonatomic, strong) NSString *preparedMessage;
+
+// Stores reference on client identifier on the
+// moment of request creation
+@property (nonatomic, copy) NSString *clientIdentifier;
+
+
+#pragma mark - Instance methods
+
+/**
+ * Retrieve message post request signature
+ */
+- (NSString *)signature;
+
+
+@end
+
+
+#pragma mark Public interface methods
+
+@implementation PNMessagePostRequest
+
+
+#pragma mark - Class methods
+
++ (PNMessagePostRequest *)postMessageRequestWithMessage:(PNMessage *)message; {
+
+    return [[[self class] alloc] initWithMessage:message];
+}
+
+
+#pragma mark - Instance methods
+
+- (id)initWithMessage:(PNMessage *)message {
+
+    // Check whether initialization was successful or not
+    if ((self = [super init])) {
+
+        self.sendingByUserRequest = YES;
+        self.message = message;
+        self.clientIdentifier = [PubNub escapedClientIdentifier];
+    }
+
+
+    return self;
+}
+
+- (NSString *)callbackMethodName {
+
+    return PNServiceResponseCallbacks.sendMessageCallback;
+}
+
+- (NSString *)resourcePath {
+
+    if (self.preparedMessage == nil) {
+
+        NSString *message = self.message.message;
+
+        // Retrieve reference on encrypted message (if possible)
+        PNError *encryptionError;
+        if ([PNCryptoHelper sharedInstance].isReady) {
+
+            message = [PubNub AESEncrypt:self.message.message error:&encryptionError];
+            
+            if (encryptionError != nil) {
+
+                PNLog(PNLogCommunicationChannelLayerErrorLevel,
+                        self,
+                        @"Message encryption failed with error: %@\nUnencrypted message will be sent.",
+                        encryptionError);
+            }
+        }
+
+        // Encode message with % so it will be delivered w/o damages to
+        // the PubNub service
+#ifndef CRYPTO_BACKWARD_COMPATIBILITY_MODE
+        self.preparedMessage = [message percentEscapedString];
+#else
+        self.preparedMessage = [message nonStringPercentEscapedString];
+#endif
+    }
+
+
+    return [NSString stringWithFormat:@"%@/publish/%@/%@/%@/%@/%@_%@/%@?uuid=%@%@",
+                    kPNRequestAPIVersionPrefix,
+                    [[PubNub sharedInstance].configuration.publishKey percentEscapedString],
+                    [[PubNub sharedInstance].configuration.subscriptionKey percentEscapedString],
+                    [self signature],
+                    [self.message.channel escapedName],
+                    [self callbackMethodName],
+                    self.shortIdentifier,
+                    self.preparedMessage,
+                    self.clientIdentifier,
+					([self authorizationField]?[NSString stringWithFormat:@"&%@", [self authorizationField]]:@"")];
+}
+
+- (NSString *)debugResourcePath {
+
+    NSMutableArray *resourcePathComponents = [[[self resourcePath] componentsSeparatedByString:@"/"] mutableCopy];
+    [resourcePathComponents replaceObjectAtIndex:2 withObject:PNObfuscateString([[PubNub sharedInstance].configuration.publishKey percentEscapedString])];
+    [resourcePathComponents replaceObjectAtIndex:3 withObject:PNObfuscateString([[PubNub sharedInstance].configuration.subscriptionKey percentEscapedString])];
+
+    return [resourcePathComponents componentsJoinedByString:@"/"];
+}
+
+- (NSString *)signature {
+
+    NSString *signature = @"0";
+#if PN_SHOULD_USE_SIGNATURE
+    NSString *secretKey = [PubNub sharedInstance].configuration.secretKey;
+    if ([secretKey length] > 0) {
+
+        NSString *signedRequestPath = [NSString stringWithFormat:@"%@/%@/%@/%@/%@%@",
+                        [PubNub sharedInstance].configuration.publishKey,
+                        [PubNub sharedInstance].configuration.subscriptionKey,
+                        secretKey,
+                        [self.message.channel escapedName],
+                        self.message.message,
+                        ([self authorizationField]?[NSString stringWithFormat:@"?%@", [self authorizationField]]:@"")];
+
+        signature = PNHMACSHA256String(secretKey, signedRequestPath);
+    }
+#endif
+
+    return signature;
+}
+
+#pragma mark -
+
+
+@end
