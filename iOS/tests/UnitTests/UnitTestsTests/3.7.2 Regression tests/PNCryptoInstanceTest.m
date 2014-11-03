8e6d066527556d038e2323b5c1ac92e39d45cf8c
Vadim Osovets <vosovets@micro-b.net>
2014-11-03 15:53:14 +0200

    Improved crypto test.

--- iOS/tests/UnitTests/UnitTestsTests/3.7.2 Regression tests/PNCryptoInstanceTest.m
+++ iOS/tests/UnitTests/UnitTestsTests/3.7.2 Regression tests/PNCryptoInstanceTest.m
@@ -46,12 +46,10 @@ static NSString *kSecretKey = nil;
 - (void)setUp {
     
     [super setUp];
-    NSLog(@"Start %@ test", self.name);
+    
+    
     [_firstUserPubNub disconnect];
     [_secondUserPubNub disconnect];
-    
-    
-// 1. First user
     
     // Configuration
     _firstUserConfiguration = [PNConfiguration configurationForOrigin:kOriginPath
@@ -59,20 +57,8 @@ static NSString *kSecretKey = nil;
                                                          subscribeKey:kSubscribeKey
                                                             secretKey:kSecretKey
                                                             cipherKey:@"enigma"];
-    
-    _firstUserConfiguration = [PNConfiguration defaultConfiguration];
-    
-    // CryptoHelper
-//    PNError *helperInitializationError = nil;
-//    _firstUserCryptoHelper = [PNCryptoHelper helperWithConfiguration:_firstUserConfiguration error:&helperInitializationError];
-//    if (helperInitializationError) {
-//        NSLog(@"%@ setup first user error: %@", self.name, helperInitializationError);
-//    }
-    
     // Connection 
     _firstUserPubNub = [PubNub clientWithConfiguration:_firstUserConfiguration andDelegate:self];
-        
-// 2. Second user
     
     // Configuration
     _secondUserConfiguration = [PNConfiguration configurationForOrigin:kOriginPath
@@ -81,22 +67,14 @@ static NSString *kSecretKey = nil;
                                                              secretKey:kSecretKey
                                                             cipherKey:@"enigma"];
     
-    // 2.2 CryptoHelper
-//    helperInitializationError = nil;
-//    _secondUserCryptoHelper = [PNCryptoHelper helperWithConfiguration:_secondUserConfiguration error:&helperInitializationError];
-//    if (helperInitializationError) {
-//        NSLog(@"%@ setup second user error: %@", self.name, helperInitializationError);
-//    }
+    _secondUserPubNub = [PubNub clientWithConfiguration:_secondUserConfiguration andDelegate:self];
     
-    // 2.3 Connection
-    _secondUserPubNub = [PubNub clientWithConfiguration:_firstUserConfiguration andDelegate:self];
     
-// 3. Array of messages
+    // test data
     _arrayMessages = [[NSMutableArray alloc] init];
     
     [_arrayMessages addObject:@""];
     [_arrayMessages addObject:@"Hello World"];
-//    [_arrayMessages addObject:@[@"seven", @"eight", @{@"food": @"Cheeseburger", @"drink": @"Coffee"}]];
     [_arrayMessages addObject:[NSString stringWithFormat:@"%@", [NSDate date]]];
 }
 
@@ -114,59 +92,6 @@ static NSString *kSecretKey = nil;
 
     [super tearDown];
 }
-
-
-// Sending crypto messages
-//- (void)tes1CryptoInstance {
-//    
-//    // Subscribing
-//    PNChannel *_testChannel = [PNChannel channelWithName:@"iosdev"];
-//    [_firstUserPubNub subscribeOn:@[_testChannel]];
-//    [_secondUserPubNub subscribeOn:@[_testChannel]];
-//    _resGroup =  dispatch_group_create();
-//    
-//    // Cycle messages
-//    for( int i=0; i<_arrayMessages.count; i++ ) {
-//        
-//        // Encrypting message
-//        PNError *processingError = nil;
-//        NSString *_encryptedMessage = [_firstUserCryptoHelper encryptedStringFromString:_arrayMessages[i] error:&processingError];
-//        XCTAssertNil( processingError, @"Error during encryption process %@", processingError);
-//        processingError = nil;
-//        
-//       // Sending message
-//        dispatch_group_enter(_resGroup);
-//        dispatch_group_enter(_resGroup);
-//        
-//        [_firstUserPubNub sendMessage:_encryptedMessage
-//                   toChannel:_testChannel
-//                  compressed:YES
-//              storeInHistory:YES
-//         withCompletionBlock:^(PNMessageState state, id message) {
-//             
-//             switch (state) {
-//                 case PNMessageSending:
-//                     NSLog(@"Message sending");
-//                 case PNMessageSendingError:
-//                     XCTFail(@"Error during sending massege occured: PNMessageSendingError");
-//                     dispatch_group_leave(_resGroup);
-//                     break;
-//                 case PNMessageSent:
-//                     NSLog(@"Sending message %@, %@",_arrayMessages[i], message);
-//                     _numberMassege = i;
-//                     dispatch_group_leave(_resGroup);
-//                     break;
-//             }
-//
-//         }];
-//        
-//        if ([GCDWrapper isGroup:_resGroup timeoutFiredValue:10]) {
-//            XCTFail(@"Cannot send message.");
-//        }
-//
-//        
-//    }
-//}
 
 - (void)testCryptoInstance {
     
@@ -287,35 +212,17 @@ static NSString *kSecretKey = nil;
     XCTFail(@"Did fail subscription on channel: %@", error);
 }
 
-
 - (void)pubnubClient:(PubNub *)client didFailMessageSend:(PNMessage *)_encryptedMessage withError:(PNError *)error {
     XCTFail(@"Did fail encrypted message send: %@", error);
 }
 
 - (void)pubnubClient:(PubNub *)client didReceiveMessage:(PNMessage *)encryptedMessage {
     
-    dispatch_group_leave(_resGroup);
-    return;
-}
-
-- (void)pubnubClient:(PubNub *)client didReceivedMessage:(PNMessage *)encryptedMessage {
-   
-    
-    dispatch_group_leave(_resGroup);
-    return;
-    
-    // Decoding message
-//    PNError *processingError = nil;
-//    NSString *_decodedMessage = [_secondUserCryptoHelper decryptedStringFromString:_arrayMessages[_numberMassege] error:&processingError];
-//    XCTAssertNil(processingError, @"Error during decoding process %@", processingError);
-//    
-//   
-//    processingError = nil;
-//    
-//    // Testing for equality sended and geted messages
-//    XCTAssertEqualObjects(_decodedMessage, encryptedMessage, @"Encrypted and decoded messages are not equal: %@", encryptedMessage);
-//    NSLog(@"Message %ld, %@", _numberMassege, encryptedMessage);
-//    
+    if ([client isEqual:_secondUserPubNub]) {
+        if (_resGroup != NULL) {
+            dispatch_group_leave(_resGroup);
+        }
+    }
 }
 
 @end
