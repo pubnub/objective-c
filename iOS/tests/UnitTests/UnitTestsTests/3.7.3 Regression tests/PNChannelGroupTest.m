a445d58090fcf1b29eb15ca1cfabb82175f62cd6
Vadim Osovets <vosovets@micro-b.net>
2014-11-10 10:59:27 +0200

    [#81762590] Improve channel group test.

--- iOS/tests/UnitTests/UnitTestsTests/3.7.3 Regression tests/PNChannelGroupTest.m
+++ iOS/tests/UnitTests/UnitTestsTests/3.7.3 Regression tests/PNChannelGroupTest.m
@@ -36,7 +36,7 @@ static NSString *kNamespaceSeparatorSymbol = @":";
 
 - (void)testCreateGroup
 {
-    NSArray *names = @[@"test", @"test:test", @"test_test:test", @"test_test:test:test"];
+    NSArray *names = @[@"test", @"group_name_test", @"test:test", @"test_test:test", @"test_test:test:test"];
     
     PNConfiguration *configuration = [PNConfiguration configurationForOrigin:kTestPNOriginHost publishKey:kTestPNPublishKey subscribeKey:kTestPNSubscriptionKey secretKey:nil];
     
@@ -51,32 +51,126 @@ static NSString *kNamespaceSeparatorSymbol = @":";
         
         dispatch_group_t testResGroup = dispatch_group_create();
         
-        dispatch_group_enter(testResGroup);
-        
         NSString *channelName = [NSString stringWithFormat:@"test_%lu", (unsigned long)[names indexOfObject:groupName]];
         
-        // it means we subscribe to group in default namespace
-        if ([groupName rangeOfString:kNamespaceSeparatorSymbol].location == NSNotFound) {
+        NSRange range = [groupName rangeOfString:kNamespaceSeparatorSymbol];
+        
+        // we try to subscribe to group in default namespace
+        if (range.location == NSNotFound) {
             
-//            [_pubNub addChannels:@[[PNChannel channelWithName:channelName]] toGroup:group withCompletionHandlingBlock:^(PNChannelGroup *channelGroup, NSArray *channels, PNError *error) {
-//                
-//                if (!error) {
-//                    // TODO: check if we really subscribed to it?
-//                } else {
-//                    XCTFail(@"Cannot add channel to group: %@ %@", channelName, groupName);
-//                }
-//                
-//                dispatch_group_leave(testResGroup);
-//            }];
-//            
+            dispatch_group_enter(testResGroup);
+            
+            [_pubNub addChannels:@[[PNChannel channelWithName:channelName]] toGroup:group withCompletionHandlingBlock:^(PNChannelGroup *channelGroup, NSArray *channels, PNError *error) {
+                
+                if (!error) {
+                    // TODO: check if we really subscribed to it?
+                } else {
+                    XCTFail(@"Cannot add channel to group: %@ %@", channelName, groupName);
+                }
+                
+                dispatch_group_leave(testResGroup);
+            }];
+            
             if ([GCDWrapper isGroup:testResGroup timeoutFiredValue:10]) {
                 XCTFail(@"Timeout is fired. Cannot add channel to group: %@ %@", channelName, groupName);
-//                dispatch_group_leave(testResGroup);
+                dispatch_group_leave(testResGroup);
             }
-//
-//            testResGroup = NULL;
-//            
-//            NSLog(@"Wait stop.");
+            
+            // check if group is created on server-side
+            
+            dispatch_group_t testResGroup2 = dispatch_group_create();
+            
+            dispatch_group_enter(testResGroup2);
+            
+            [_pubNub requestDefaultChannelGroupsWithCompletionHandlingBlock:^(NSString *namespaceName, NSArray *channelGroups, PNError *error) {
+                if (!error) {
+                    BOOL isGroupFound = NO;
+                    
+                    for (PNChannelGroup *group in channelGroups) {
+                        if ([groupName isEqualToString:group.groupName]) {
+                            isGroupFound = YES;
+                            break;
+                        }
+                    }
+                    
+                    if (!isGroupFound) {
+                        XCTFail(@"Cannot find group in default namespace: %@ group: %@", namespaceName, groupName);
+                    }
+                    
+                } else {
+                    XCTFail(@"Cannot request channel groups in default namespace: %@", namespaceName);
+                }
+                
+                dispatch_group_leave(testResGroup2);
+            }];
+            
+            if ([GCDWrapper isGroup:testResGroup2 timeoutFiredValue:5]) {
+                XCTFail(@"Timeout is fired. Cannot add channel to group: %@ %@", channelName, groupName);
+                dispatch_group_leave(testResGroup2);
+            }
+            
+        } else {
+            // try to determine group and namespace:
+            NSString *testNamespace = [groupName substringToIndex:range.location];
+            NSString *testGroupName = [groupName substringFromIndex:range.location + 1];
+            
+            // we try to subscribe to group in a namespace
+            
+            dispatch_group_enter(testResGroup);
+            
+            [_pubNub addChannels:@[[PNChannel channelWithName:channelName]] toGroup:group withCompletionHandlingBlock:^(PNChannelGroup *channelGroup, NSArray *channels, PNError *error) {
+                
+                if (!error) {
+                    // TODO: check if we really subscribed to it?
+                } else {
+                    XCTFail(@"Cannot add channel to group: %@ %@", channelName, groupName);
+                }
+                
+                if ([testNamespace isEqualToString:@"test_test"]) {
+                    NSLog(@"");
+                }
+                
+                dispatch_group_leave(testResGroup);
+            }];
+            
+            if ([GCDWrapper isGroup:testResGroup timeoutFiredValue:10]) {
+                XCTFail(@"Timeout is fired. Cannot add channel to group: %@ %@", channelName, groupName);
+                dispatch_group_leave(testResGroup);
+            }
+            
+            // check if group is created on server-side
+            
+            dispatch_group_t testResGroup2 = dispatch_group_create();
+            
+            dispatch_group_enter(testResGroup2);
+            
+            [_pubNub requestChannelGroupsForNamespace:testNamespace
+                          withCompletionHandlingBlock:^(NSString *namespaceName, NSArray *channelGroups, PNError *error) {
+                if (!error) {
+                    BOOL isGroupFound = NO;
+                    
+                    for (PNChannelGroup *group in channelGroups) {
+                        if ([testGroupName isEqualToString:group.groupName]) {
+                            isGroupFound = YES;
+                            break;
+                        }
+                    }
+                    
+                    if (!isGroupFound) {
+                        XCTFail(@"Cannot find group in namespace: %@ group: %@", namespaceName, groupName);
+                    }
+                    
+                } else {
+                    XCTFail(@"Cannot request channel groups in namespace: %@", namespaceName);
+                }
+                
+                dispatch_group_leave(testResGroup2);
+            }];
+            
+            if ([GCDWrapper isGroup:testResGroup2 timeoutFiredValue:5]) {
+                XCTFail(@"Timeout is fired. Cannot add channel to group: %@ %@", channelName, groupName);
+                dispatch_group_leave(testResGroup2);
+            }
         }
     }
 }
