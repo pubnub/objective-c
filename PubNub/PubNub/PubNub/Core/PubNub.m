9a888bd28ae24c7215817556d5ab2822e63b0262
Vadim Osovets <vosovets@micro-b.net>
2015-03-05 22:18:53 +0200

    Update Changelog, podspec.

--- PubNub/PubNub/PubNub/Core/PubNub.m
+++ PubNub/PubNub/PubNub/Core/PubNub.m
@@ -56,7 +56,7 @@
 /**
  Name of the branch which is used to store current codebase.
  */
-static NSString * const kPNCodebaseBranch = @"feature-pt88355632";
+static NSString * const kPNCodebaseBranch = @"master";
 
 /**
  SHA of the commit which stores actual changes in this codebase.
