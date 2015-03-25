54f3c1a89fcf3aaf493aa496fe880c665549d1e4
Sergey Mamontov <parfeon@me.com>
2015-03-25 23:29:03 +0200

    [#91139226] * fixed crash caused by incomlete network layer configuration on moment when it should tear down connection by request (appear only for deplyment target version below 6.x)

--- PubNub/PubNub/PubNub/Misc/PNConstants.h
+++ PubNub/PubNub/PubNub/Misc/PNConstants.h
@@ -17,7 +17,7 @@
 #pragma mark General information constants
 
 // Stores client library version number
-static NSString * const kPNLibraryVersion = @"3.7.10.1";
+static NSString * const kPNLibraryVersion = @"3.7.10.2";
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED
 static NSString * const kPNClientName = @"ObjC-iOS";
