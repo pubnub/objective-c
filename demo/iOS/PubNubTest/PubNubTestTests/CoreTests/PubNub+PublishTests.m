d72015370e39668c814482a5052ea4abd8c081a8
Vadim Osovets <vosovets@micro-b.net>
2015-05-27 19:35:57 +0300

    Add basic unit-tests templates for PubNub+Publish

--- demo/iOS/PubNubTest/PubNubTestTests/CoreTests/PubNub+PublishTests.m
+++ demo/iOS/PubNubTest/PubNubTestTests/CoreTests/PubNub+PublishTests.m
@@ -13,6 +13,12 @@
 
 #import "TestConfigurator.h"
 
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
 @interface PubNub_PublishTests : XCTestCase
 
 @end
@@ -22,32 +28,195 @@
 }
 
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    _pubNub = [PubNub clientWithPublishKey:[[TestConfigurator shared] mainPubKey] andSubscribeKey:[[TestConfigurator shared] mainSubKey]];
+    [super setUp];
+    _pubNub = [PubNub clientWithPublishKey:[[TestConfigurator shared] mainPubKey]
+                           andSubscribeKey:[[TestConfigurator shared] mainSubKey]];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     
     _pubNub = nil;
-    
     [super tearDown];
 }
 
-#pragma mark - Tests
+#pragma mark - Tests Messages (compressed:NO)
 
 - (void)testPublishNilMessage {
-    // This is an example of a functional test case.
     
     [_pubNub publish:nil
-           toChannel:[TestConfigurator uniqueString] mobilePushPayload:nil
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+       }];
+}
+
+- (void)testPublishEmptyMessage {
+    
+    [_pubNub publish:@""
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
       storeInHistory:NO
           compressed:NO
       withCompletion:^(PNStatus *status) {
           
       }];
+}
+
+- (void)testPublishHugeMessage {
+    
+    [_pubNub publish:[self randomStringWithLength:40000]
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishWeirdMessage {
+    
+    [_pubNub publish:@"WeirdMessage: /?#[]@!$&’()*+,;="
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+
+#pragma mark - Tests Messages (compressed:YES)
+
+- (void)testPublishCompressedNilMessage {
+    
+    [_pubNub publish:nil
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:YES
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishCompressedEmptyMessage {
+    
+    [_pubNub publish:@""
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:YES
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishCompressedHugeMessage {
+    
+    [_pubNub publish:[self randomStringWithLength:40000]
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:YES
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishCompressedWeirdMessage {
+    
+    [_pubNub publish:@"WeirdMessage: /?#[]@!$&’()*+,;="
+           toChannel:[TestConfigurator uniqueString]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:YES
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+#pragma mark - Tests Channels
+
+- (void)testPublishMessageToNilChannel {
+    
+    [_pubNub publish:[TestConfigurator uniqueString]
+           toChannel:nil
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishMessageToNotStringChannels {
+    
+    NSNumber *number = @5;
+    
+    [_pubNub publish:[TestConfigurator uniqueString]
+           toChannel:(NSString *)number
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishMessageToNotValidChannel {
+    
+    [_pubNub publish:[TestConfigurator uniqueString]
+           toChannel:@""
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishMessageToChannelWithLongName {
+    
+    [_pubNub publish:@""
+           toChannel:[self randomStringWithLength:1000]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+- (void)testPublishMessageToChannels {
+    
+    [_pubNub publish:[TestConfigurator uniqueString]
+           toChannel:[NSString stringWithFormat:@"%@,%@", [TestConfigurator uniqueString], [TestConfigurator uniqueString]]
+   mobilePushPayload:nil
+      storeInHistory:NO
+          compressed:NO
+      withCompletion:^(PNStatus *status) {
+          
+      }];
+}
+
+
+#pragma mark - Private methods
+
+-(NSString *)randomStringWithLength:(int)length {
+    
+    NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    NSMutableString *randomString = [NSMutableString stringWithCapacity:length];
+    
+    for (int i = 0; i < length; i++) {
+        
+        [randomString appendFormat: @"%C",[letters characterAtIndex:(NSUInteger)arc4random_uniform((u_int32_t)[letters length])]];
+    }
+    return randomString;
 }
 
 @end
