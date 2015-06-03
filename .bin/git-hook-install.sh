bc466bdae876347d0abd49c2fcbd889ed3c52cc5
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-03 15:25:19 -0700

    Removed unused bin and travis files from git

--- .bin/git-hook-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Retrieve path to the folder where script is stored.
-SCRIPT_FOLDER_PATH=$( cd "$( dirname "$0" )" && pwd )
-
-cp "${SCRIPT_FOLDER_PATH}/commit-message-prepare.sh" "${SCRIPT_FOLDER_PATH}/../.git/hooks/prepare-commit-msg"
-chmod +x "${SCRIPT_FOLDER_PATH}/../.git/hooks/prepare-commit-msg"
