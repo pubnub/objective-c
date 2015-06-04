54aac21e470e96600960636f4e368564f2422c2b
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-03 17:41:56 -0700

    Revert "Exposed designated initializer for PNConfiguration"
    This reverts commit 8ca58014d8adaece2ee8996175a5c79d05efcddc.

--- Pod/Classes/Data/PNConfiguration.m
+++ Pod/Classes/Data/PNConfiguration.m
@@ -17,6 +17,20 @@
 
 @property (nonatomic, copy) NSString *deviceID;
 
+/**
+ @brief  Initialize configuration instance using minimal required data.
+ 
+ @param publishKey   Key which allow client to use data push API.
+ @param subscribeKey Key which allow client to subscribe on live feeds pushed from \b PubNub 
+                     service.
+ 
+ @return Configured and ready to se configuration instance.
+ 
+ @since 4.0
+ */
+- (instancetype)initWithPublishKey:(NSString *)publishKey
+                      subscribeKey:(NSString *)subscribeKey NS_DESIGNATED_INITIALIZER;
+
 #pragma mark -
 
 
