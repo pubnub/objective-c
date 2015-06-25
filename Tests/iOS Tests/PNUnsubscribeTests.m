d30d780c7bdb1b36abcdfed8656278184c6e8583
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-25 10:09:44 -0700

    Longer timeout on unsubscribe.
    Misunderstanding of how the API works, updated test to reflect
    new knowledge

--- Tests/iOS Tests/PNUnsubscribeTests.m
+++ Tests/iOS Tests/PNUnsubscribeTests.m
@@ -28,7 +28,7 @@
     self.settingUp = YES;
     self.subscribeExpectation = [self expectationWithDescription:@"subscribe"];
     [self.client subscribeToChannels:@[@"a"] withPresence:YES];
-    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+    [self waitForExpectationsWithTimeout:15 handler:^(NSError *error) {
         NSLog(@"error: %@", error);
     }];
     
