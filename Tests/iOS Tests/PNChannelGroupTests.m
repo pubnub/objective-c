c1648ac640f7bc0ee8d83049d812a93a6c14d72b
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-28 18:22:18 -0700

    Basic channel group getter setter tests

--- Tests/iOS Tests/PNChannelGroupTests.m
+++ Tests/iOS Tests/PNChannelGroupTests.m
@@ -10,11 +10,12 @@
 #import <XCTest/XCTest.h>
 #import <PubNub/PubNub.h>
 
-#import "PNBasicSubscribeTestCase.h"
+#import "PNBasicClientTestCase.h"
+typedef void (^PNChannelGroupAssertions)(PNAcknowledgmentStatus *status);
 
-static NSString * const kChannelGroup = @"79713A48-107C-4338-9977-92EEC1F29577";
+static NSString * const kPNChannelGroupTestsName = @"PNChannelGroupTestsName";
 
-@interface PNChannelGroupTests : PNBasicSubscribeTestCase
+@interface PNChannelGroupTests : PNBasicClientTestCase
 @end
 
 @implementation PNChannelGroupTests
@@ -23,34 +24,155 @@ static NSString * const kChannelGroup = @"79713A48-107C-4338-9977-92EEC1F29577";
     return NO;
 }
 
+- (void)setUp {
+    [super setUp];
+    [self performVerifiedRemoveAllChannelsFromGroup:kPNChannelGroupTestsName withAssertions:nil];
+}
+
 - (void)tearDown {
-    XCTestExpectation *channelGroupTearDownExpectation = [self expectationWithDescription:@"tearDownExpectation"];
-    [self.client removeChannelsFromGroup:kChannelGroup
-                          withCompletion:^(PNAcknowledgmentStatus *status) {
-        [channelGroupTearDownExpectation fulfill];
-    }];
-    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
-        NSLog(@"error: %@", error);
-    }];
+    [self performVerifiedRemoveAllChannelsFromGroup:kPNChannelGroupTestsName withAssertions:nil];
     [super tearDown];
 }
 
 - (void)testChannelGroupAdd {
-    self.subscribeExpectation = [self expectationWithDescription:@"network"];
-    [self.client addChannels:@[@"a", @"c"] toGroup:kChannelGroup
-              withCompletion:^(PNAcknowledgmentStatus *status) {
+    PNWeakify(self);
+    [self performVerifiedAddChannels:@[@"a", @"c"] toGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
         XCTAssertNotNil(status);
         XCTAssertFalse(status.isError);
         XCTAssertEqual(status.operation, PNAddChannelsToGroupOperation);
         XCTAssertEqual(status.category, PNAcknowledgmentCategory);
         XCTAssertEqual(status.statusCode, 200);
-        [self.subscribeExpectation fulfill];
+    }];
+}
+
+- (void)testChannelsForGroup {
+    PNWeakify(self);
+    NSArray *channelGroups = @[@"a", @"c"];
+    [self performVerifiedAddChannels:channelGroups toGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(status);
+        XCTAssertFalse(status.isError);
+        XCTAssertEqual(status.operation, PNAddChannelsToGroupOperation);
+        XCTAssertEqual(status.category, PNAcknowledgmentCategory);
+        XCTAssertEqual(status.statusCode, 200);
+    }];
+    
+    XCTestExpectation *channelsForGroup = [self expectationWithDescription:@"channelsForGroup"];
+    [self.client channelsForGroup:kPNChannelGroupTestsName withCompletion:^(PNChannelGroupChannelsResult *result, PNErrorStatus *status) {
+        PNStrongify(self);
+        XCTAssertNil(status);
+        XCTAssertNotNil(result);
+        XCTAssertEqual(result.statusCode, 200);
+        XCTAssertEqual(result.operation, PNChannelsForGroupOperation);
+        XCTAssertEqualObjects(result.data.channels, channelGroups);
+        [channelsForGroup fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+
+- (void)testChannelGroupRemoveAll {
+    PNWeakify(self);
+    [self performVerifiedAddChannels:@[@"a", @"c"] toGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(status);
+        XCTAssertFalse(status.isError);
+        XCTAssertEqual(status.operation, PNAddChannelsToGroupOperation);
+        XCTAssertEqual(status.category, PNAcknowledgmentCategory);
+        XCTAssertEqual(status.statusCode, 200);
+    }];
+    
+    [self performVerifiedRemoveAllChannelsFromGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(status);
+        XCTAssertFalse(status.isError);
+        XCTAssertEqual(status.operation, PNRemoveGroupOperation);
+        XCTAssertEqual(status.category, PNAcknowledgmentCategory);
+        XCTAssertEqual(status.statusCode, 200);
+        
+    }];
+    
+}
+
+- (void)testRemoveSpecificChannelsFromGroup {
+    PNWeakify(self);
+    [self performVerifiedAddChannels:@[@"a", @"c"] toGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(status);
+        XCTAssertFalse(status.isError);
+        XCTAssertEqual(status.operation, PNAddChannelsToGroupOperation);
+        XCTAssertEqual(status.category, PNAcknowledgmentCategory);
+        XCTAssertEqual(status.statusCode, 200);
+    }];
+    [self performVerifiedRemoveChannels:@[@"a"] fromGroup:kPNChannelGroupTestsName withAssertions:^(PNAcknowledgmentStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(status);
+        XCTAssertFalse(status.isError);
+        XCTAssertEqual(status.operation, PNRemoveChannelsFromGroupOperation);
+        XCTAssertEqual(status.category, PNAcknowledgmentCategory);
+        XCTAssertEqual(status.statusCode, 200);
+    }];
+}
+
+- (void)testGetAllChannelGroupsForClient {
+    PNWeakify(self);
+    XCTestExpectation *getAllChannelGroups = [self expectationWithDescription:@"getAllChannelGroups"];
+    [self.client channelGroupsWithCompletion:^(PNChannelGroupsResult *result, PNErrorStatus *status) {
+        PNStrongify(self);
+        XCTAssertNotNil(result);
+        XCTAssertNil(status);
+        XCTAssertEqual(result.operation, PNChannelGroupsOperation);
+        XCTAssertEqual(result.statusCode, 200);
+        // TODO: assert on actual groups, for now just do count
+        XCTAssertEqual(result.data.groups.count, 64);
+        [getAllChannelGroups fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+
+#pragma mark - Helpers
+
+- (void)performVerifiedAddChannels:(NSArray *)channels toGroup:(NSString *)channelGroup withAssertions:(PNChannelGroupAssertions)assertions {
+    XCTestExpectation *addChannelsToGroupExpectation = [self expectationWithDescription:@"addChannels"];
+    [self.client addChannels:channels toGroup:channelGroup
+              withCompletion:^(PNAcknowledgmentStatus *status) {
+                  if (assertions) {
+                      assertions(status);
+                  }
+                  [addChannelsToGroupExpectation fulfill];
+              }];
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+
+- (void)performVerifiedRemoveAllChannelsFromGroup:(NSString *)channelGroup withAssertions:(PNChannelGroupAssertions)assertions {
+    XCTestExpectation *removeChannels = [self expectationWithDescription:@"removeChannels"];
+    [self.client removeChannelsFromGroup:channelGroup withCompletion:^(PNAcknowledgmentStatus *status) {
+        if (assertions) {
+            assertions(status);
+        }
+        [removeChannels fulfill];
     }];
     [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
-        if (error) {
-            NSLog(@"error: %@", error);
-            XCTFail(@"what went wrong?");
+        XCTAssertNil(error);
+    }];
+}
+
+- (void)performVerifiedRemoveChannels:(NSArray *)channels fromGroup:(NSString *)channelGroup withAssertions:(PNChannelGroupAssertions)assertions {
+    XCTestExpectation *removeSpecificChannels = [self expectationWithDescription:@"removeSpecificChannels"];
+    [self.client removeChannels:channels fromGroup:channelGroup withCompletion:^(PNAcknowledgmentStatus *status) {
+        if (assertions) {
+            assertions(status);
         }
+        [removeSpecificChannels fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        XCTAssertNil(error);
     }];
 }
 
