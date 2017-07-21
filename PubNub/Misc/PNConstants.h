71ba6be5429cec183b916ddcf714d30050703907
Sergey <parfeon@me.com>
2017-07-21 23:34:21 +0200

    PubNub SDK 4.6.3 Release (#340)
    * Silence Xcode 8.3.2 warnings for wrong param names in inline docs
    
    * . added new error category PNRequestURITooLongCategory to properly handle and report issues to callbacks and completion blocks (CE-2974)
    
    * + added client 'telemetry' information processing (CE-3013)
    
    * Removed unsubscribe request cancellation by following subscribe request (#333)
    
    * ^ removed unsubscribe request cancellation by sequential call to subscribe API (CE-2977)
    
    * . reorganized code which is responsible for subscribe requests cancellation.
    
    * ^ fixed issue with macOS Keychain access in multi-user environment when none authorized (CE-3019) (#335)
    
    * ^ fixed issue with channel groups subscription (CE-3045) (#339)
    
    * . prepared for 4.6.3 release

--- PubNub/Misc/PNConstants.h
+++ PubNub/Misc/PNConstants.h
@@ -15,10 +15,10 @@
 #pragma mark General information constants
 
 // Stores client library version number
-static NSString * const kPNLibraryVersion = @"4.6.2";
+static NSString * const kPNLibraryVersion = @"4.6.3";
 
 // Stores information about SDK codebase
-static NSString * const kPNCommit = @"424bf7129cb41ecefc5045e825839f298b9f789e";
+static NSString * const kPNCommit = @"718ffe6d0f50d22ec87aa13b0b15e8df2dbd044d";
 
 /**
  @brief  Stores reference on unique identifier which is used to identify \b PubNub client among other 
