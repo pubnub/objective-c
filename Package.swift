db28761227496a679ba67a9febd70c04162173a6
Serhii Mamontov <sergey@pubnub.com>
2024-04-25 14:35:24 +0300

    wip(spm): update Package.swift

--- Package.swift
+++ Package.swift
@@ -42,21 +42,22 @@ let package = Package(
     .watchOS(.v4)
   ],
   products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
       name: "PubNub",
       targets: ["PubNub"]
     )
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-  ],
+  dependencies: [],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "PubNub",
-      dependencies: []
+      dependencies: [],
+      path: "Sources/PubNub",
+      publicHeadersPath: "include",
+      cSettings: [
+          .headerSearchPath("include"),
+          .define("PUBLIC_HEADER", to: "include"),
+      ]
     )
   ],
   swiftLanguageVersions: [.v5]
